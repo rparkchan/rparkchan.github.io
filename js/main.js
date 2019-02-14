fa739e750d37ae5b82cd727066fed9cc34d5512d
rparkchan <rparkchan@college.harvard.edu>
2019-02-13 22:34:01 -0500

    removed unnecessary render call

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ function resizeWindow() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.render(scene, camera);
 }
 
 function createLights() { 
